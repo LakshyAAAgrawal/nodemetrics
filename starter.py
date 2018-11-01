4fb88a38fa90c0c3643809e866be38752d08ebe2
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2018-11-01 20:42:08 +0530

    Initial State

---
+++ starter.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+
+import re
+import itertools
+
+ROLLNUM_REGEX = "201[0-9]{4}"
+
+class Graph(object):
+    name = "Lakshya A Agrawal"
+    email = "lakshya18242@iiitd.ac.in"
+    roll_num = "2018242"
+
+    def __init__ (self, vertices, edges):
+        """
+        Initializes object for the class Graph
+
+        Args:
+            vertices: List of integers specifying vertices in graph
+            edges: List of 2-tuples specifying edges in graph
+        """
+
+        self.vertices = vertices.sort()
+        
+        ordered_edges = list(map(lambda x: (min(x), max(x)), edges))
+        
+        self.edges    = ordered_edges
+        
+        self.validate()
+
+    def validate(self):
+        """
+        Validates if Graph if valid or not
+
+        Raises:
+            Exception if:
+                - Name is empty or not a string
+                - Email is empty or not a string
+                - Roll Number is not in correct format
+                - vertices contains duplicates
+                - edges contain duplicates
+                - any endpoint of an edge is not in vertices
+        """
+
+        if (not isinstance(self.name, str)) or self.name == "":
+            raise Exception("Name can't be empty")
+
+        if (not isinstance(self.email, str)) or self.email == "":
+            raise Exception("Email can't be empty")
+
+        if (not isinstance(self.roll_num, str)) or (not re.match(ROLLNUM_REGEX, self.roll_num)):
+            raise Exception("Invalid roll number, roll number must be a string of form 201XXXX. Provided roll number: {}".format(self.roll_num))
+
+        if not all([isinstance(node, int) for node in self.vertices]):
+            raise Exception("All vertices should be integers")
+
+        elif len(self.vertices) != len(set(self.vertices)):
+            duplicate_vertices = set([node for node in self.vertices if self.vertices.count(node) > 1])
+
+            raise Exception("Vertices contain duplicates.\nVertices: {}\nDuplicate vertices: {}".format(vertices, duplicate_vertices))
+
+        edge_vertices = list(set(itertools.chain(*self.edges)))
+
+        if not all([node in self.vertices for node in edge_vertices]):
+            raise Exception("All endpoints of edges must belong in vertices")
+
+        if len(self.edges) != len(set(self.edges)):
+            duplicate_edges = set([edge for edge in self.edges if self.edges.count(edge) > 1])
+
+            raise Exception("Edges contain duplicates.\nEdges: {}\nDuplicate vertices: {}".format(edges, duplicate_edges))
+
+    def min_dist(self, start_node, end_node):
+        '''
+        Finds minimum distance between start_node and end_node
+
+        Args:
+            start_node: Vertex to find distance from
+
+           end_node: Vertex to find distance to
+
+        Returns:
+            An integer denoting minimum distance between start_node
+            and end_node
+        '''
+        
+        raise NotImplementedError
+
+    def all_shortest_paths(start_node, end_node):
+        """
+        Finds all shortest paths between start_node and end_node
+
+        Args:
+            start_node: Starting node for paths
+            end_node: Destination node for paths
+
+        Returns:
+            A list of path, where each path is a list of integers.
+        """
+
+        raise NotImplementedError
+
+    def all_paths(node, destination, dist, path):
+        """
+        Finds all paths from node to destination with length = dist
+
+        Args:
+            node: Node to find path from
+            destination: Node to reach
+            dist: Allowed distance of path
+            path: path already traversed
+
+        Returns:
+            List of path, where each path is list ending on destination
+
+            Returns None if there no paths
+        """
+        
+        raise NotImplementedError
+
+    def betweenness_centrality(self, node):
+        """
+        Find betweenness centrality of the given node
+
+        Args:
+            node: Node to find betweenness centrality of.
+
+        Returns:
+            Single floating point number, denoting betweenness centrality
+            of the given node
+        """
+
+        raise NotImplementedError
+
+    def top_k_betweenness_centrality(self):
+        """
+        Find top k nodes based on highest equal betweenness centrality.
+
+        
+        Returns:
+            List a integer, denoting top k nodes based on betweenness
+            centrality.
+        """
+
+        raise NotImplementedError
+
+if __name__ == "__main__":
+    vertices = [1, 2, 3, 4, 5, 6]
+    edges    = [(1, 2), (1, 5), (2, 3), (2, 5), (3, 4), (4, 5), (4, 6)]
+
+    graph = Graph(vertices, edges)
